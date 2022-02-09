57c0a92b4001097b4c01857231ab01a48d9ec43d
AkbarKamran007 <93193929+AkbarKamran007@users.noreply.github.com>
2022-02-10 00:00:19 +0500

    g

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const apps = new App().app;
 var httpServer = http.createServer(apps);
 
 const start = () => {
-  httpServer.listen(8080, () => {
+  httpServer.listen(3000, () => {
     console.log(
       "Server is running:  http://localhost:8080/api/sticker",
       "Param:keyword = cute"
